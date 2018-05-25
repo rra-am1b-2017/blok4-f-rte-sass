3cc62fa3cbcf1a9393448c0bbc85db1886c17d3f
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2018-05-25 11:07:13 +0200

    Einde les

---
+++ carousel.php
@@ -0,0 +1,42 @@
+<article class="row">
+  <section class="col-12">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-50 h-50 mx-auto" src="./images/dagobertduck.jpg" alt="First slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Dagobert Duck</h5>
+            <p>Dagobert Duck is een held</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-50 h-50 mx-auto" src="./images/kwikkwekkwak.png" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Kwik Kwek en Kwak</h5>
+            <p>Dit zijn de neefjes van Donald Duck</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-50 h-50 mx-auto" src="./images/magica.png" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Zwarte Magica</h5>
+            <p>Dit is een bekende tovenaar in de Vesuvius</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+</article>
---
+++ content.php
@@ -0,0 +1,62 @@
+<article class="row">
+  <section class="col-12 col-md-6 col-lg-4 col-xl-2">
+    <div class="card text-center">
+        <div class="card-body">
+          <img src="./images/blauw.png" alt="blauw">
+        </div>
+        <div class="card-footer text-muted">
+          Dit is een mooi vierkant
+        </div>
+    </div>        
+  </section>
+  <section class="col-6 col-md-6 col-lg-4 col-xl-2">
+    <div class="card text-center">
+        <div class="card-body">
+          <img src="./images/bruin.png" alt="bruin">
+        </div>
+        <div class="card-footer text-muted">
+          Dit is een mooi vierkant
+        </div>
+    </div>        
+  </section>
+  <section class="col-6 col-md-12 col-lg-4 col-xl-2">
+    <div class="card text-center">
+        <div class="card-body">
+          <img src="./images/geel.png" alt="geel">
+        </div>
+        <div class="card-footer text-muted">
+          Dit is een mooi vierkant
+        </div>
+    </div>        
+  </section>
+  <section class="col-12 col-md-6 col-lg-4 col-xl-2">
+    <div class="card text-center">
+        <div class="card-body">
+          <img src="./images/goud.png" alt="goud">
+        </div>
+        <div class="card-footer text-muted">
+          Dit is een mooi vierkant
+        </div>
+    </div>        
+  </section>
+  <section class="col-6 col-md-6 col-lg-4 col-xl-2">
+    <div class="card text-center">
+        <div class="card-body">
+          <img src="./images/grijs.png" alt="grijs">
+        </div>
+        <div class="card-footer text-muted">
+          Dit is een mooi vierkant
+        </div>
+    </div>        
+  </section>
+  <section class="col-6 col-md-12 col-lg-4 col-xl-2">
+    <div class="card text-center">
+        <div class="card-body">
+          <img src="./images/groen.png" alt="groen">
+        </div>
+        <div class="card-footer text-muted">
+          Dit is een mooi vierkant
+        </div>
+    </div>        
+  </section>
+</article>
