890f33060858e74b64abd213dbddd1c3b10c08b4
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2018-05-25 08:49:02 +0200

    plaatjes

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
   <body>
     <div class="container-fluid">Zet hier je navbar</div>
     <div class="container">
+      <article>
       <article class="row">
         <section class="col-12 col-md-6 col-lg-4 col-xl-2">
           <div class="card text-center">
